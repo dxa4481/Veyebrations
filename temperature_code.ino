6c00b0095e14ab982b882dad6c6d4ada99507edc
DYLAN AYREY <dxa4481@kgcoe-tools-03.main.ad.rit.edu>
2015-08-26 14:35:51 -0400

    added temperature code

---
+++ temperature_code.ino
@@ -0,0 +1,20 @@
+#include <math.h>
+
+double getTemperature() {
+  double Temp;
+  int RawADC = analogRead(0);
+  Temp = log(10000.0*((1024.0/RawADC-1))); // for pull-down configuration
+  //    =log(10000.0/(1024.0/RawADC-1)) // for pull-up configuration
+  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * sq(Temp) ))* Temp );
+  Temp = Temp - 273.15;            // Convert Kelvin to Celcius
+  return Temp;
+}
+
+void setup() {
+   Serial.begin(115200);
+
+}
+
+void loop() {
+  getTemperature()
+}
